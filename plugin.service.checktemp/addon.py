e85624f9599bf43462c0e4dcc2af4be475a4b44e
Davide Fresilli <davidedj10@live.it>
2016-06-21 14:03:30 +0200

    added corev cpu percentage and armfreq

--- plugin.service.checktemp/addon.py
+++ plugin.service.checktemp/addon.py
@@ -2,8 +2,17 @@ import xbmcaddon
 import socket
 import commands
 import subprocess
- 
 
+
+def cpusage():
+
+    
+    usg = commands.getstatusoutput("top -bn1 | awk 'FNR == 2 {print $2}'")
+    
+    value = usg[1]
+
+    return(str(value))
+ 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind(('', 8080))
@@ -14,16 +23,17 @@ while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
     
-    p = subprocess.Popen("cat /sys/class/thermal/thermal_zone0/temp", stdout=subprocess.PIPE, shell=True)
+    p = subprocess.Popen("cat /sys/class/thermal/thermal_zone0/temp && vcgencmd measure_volts core && vcgencmd measure_clock arm", stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
+    
     
     print request
 
     http_response = """\
 HTTP/1.1 200 OK
 
-""" + output
+""" + output + cpusage()
 
     client_connection.sendall(http_response)
     client_connection.close()
