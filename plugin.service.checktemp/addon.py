538b6779278b366f4a7067619fc991db6e637509
Davide Fresilli <davidedj10@live.it>
2016-06-21 15:10:45 +0200

    defined all the functions and cleaned some brackets v1.0.1

--- plugin.service.checktemp/addon.py
+++ plugin.service.checktemp/addon.py
@@ -4,15 +4,47 @@ import commands
 import subprocess
 
 
+def httpresp():
+
+    httpresponse = """\
+HTTP/1.1 200 OK
+
+"""
+    return(httpresponse)
+
 def cpusage():
 
-    
     usg = commands.getstatusoutput("top -bn1 | awk 'FNR == 2 {print $2}'")
     
     value = usg[1]
 
     return(str(value))
- 
+
+
+def coretemp():
+
+    rawtemp = commands.getstatusoutput("cat /sys/class/thermal/thermal_zone0/temp")
+
+    temp = rawtemp[1]
+
+    return(str(temp))
+
+def corevolts(): 
+
+    rawcorevolts = commands.getstatusoutput("vcgencmd measure_volts core | awk '{print substr($1, 6, 6)}'")
+
+    corevolts = rawcorevolts[1]
+
+    return(str(corevolts))
+
+def coreclock():
+
+    rawcoreclock = commands.getstatusoutput("vcgencmd measure_clock arm | awk '{print substr($1, 15)}'")
+
+    coreclock = rawcoreclock[1]
+
+    return(str(coreclock))
+
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind(('', 8080))
@@ -22,18 +54,10 @@ print 'Serving HTTP on port %s ...' % 8080
 while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
-    
-    p = subprocess.Popen("cat /sys/class/thermal/thermal_zone0/temp && vcgencmd measure_volts core && vcgencmd measure_clock arm", stdout=subprocess.PIPE, shell=True)
-    (output, err) = p.communicate()
-    p_status = p.wait()
-    
-    
+
     print request
 
-    http_response = """\
-HTTP/1.1 200 OK
-
-""" + output + cpusage()
+    http_response = httpresp() + coretemp() + "\n" + corevolts() + "\n" + coreclock() + "\n" + cpusage()
 
     client_connection.sendall(http_response)
     client_connection.close()
